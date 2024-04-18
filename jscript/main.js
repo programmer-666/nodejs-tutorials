48d11f4fd505426efaae96bd1beeda797672b538
programmer-666 <34501436+programmer-666@users.noreply.github.com>
2024-04-18 16:39:43 +0300

    update

--- jscript/main.js
+++ jscript/main.js
@@ -1,4 +1,24 @@
+/*
 alert("hello");
 var x = prompt("Type thing...");
 alert(x.length);
 alert(x.slice(x.length - 2, x.length).toUpperCase().toLowerCase());
+*/
+
+var outLabel = document.getElementById("lOut");
+
+// outLabel.innerHTML = Math.PI ** Math.PI;
+
+const pisqrtn = (n) => {
+    return Math.PI * Math.PI + n;
+};
+
+const floor = () => {
+    outLabel.innerHTML = Math.floor(outLabel.innerHTML);
+}
+
+function increment() {
+    outLabel.innerHTML = pisqrtn(parseFloat(outLabel.innerHTML));
+};
+
+outLabel.innerHTML = pisqrtn(0);
