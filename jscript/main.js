a152091885e1ed616b80a7c8fd62ea8a7d459baf
programmer-666 <34501436+programmer-666@users.noreply.github.com>
2024-04-19 09:26:07 +0300

    update

--- jscript/main.js
+++ jscript/main.js
@@ -6,7 +6,7 @@ alert(x.slice(x.length - 2, x.length).toUpperCase().toLowerCase());
 */
 
 var outLabel = document.getElementById("lOut");
-
+var rands = [];
 // outLabel.innerHTML = Math.PI ** Math.PI;
 
 const pisqrtn = (n) => {
@@ -16,7 +16,11 @@ const pisqrtn = (n) => {
 const floor = () => {
     outLabel.innerHTML = Math.floor(outLabel.innerHTML);
 }
-
+const createRandom = () => {
+    rands.push(Math.floor(Math.random() * 100));
+    outLabel.innerHTML = rands[rands.length - 1] 
+    console.log(rands);
+}
 function increment() {
     outLabel.innerHTML = pisqrtn(parseFloat(outLabel.innerHTML));
 };
