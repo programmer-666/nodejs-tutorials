46cc2726c5619f5f47bb6d01bd4f1db0c03f6c40
programmer-666 <34501436+programmer-666@users.noreply.github.com>
2024-04-30 17:46:21 +0300

    update

--- wsiteTest/app.js
+++ wsiteTest/app.js
@@ -1,18 +1,33 @@
+import bodyParser from "body-parser";
 import express from "express";
 import morgan from "morgan";
 import fs from "fs";
 
 
 const app = express();
+
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+    res.locals = {
+        test: "t123$"
+    };
+
     fs.readFile("public/header.html", "utf-8", (err, data) => {
         res.render("index.ejs", {
             headerTags: data
         });
-        console.log(err);
     })
 });
+
+app.get("/ptest", (req, res) => {
+    res.render("ptest.ejs");
+});
+app.post("/ptest", (req, res) => {
+    res.send("ok");
+    console.log(req.body["val"]);
+});
+
 
 app.listen(80);
